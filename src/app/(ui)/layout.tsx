c4cc5cf7c8e5a0151b1683d5186d85216b886a0c
ecv-rijen <rijen.maharjan@classifiedsventures.com>
2024-06-20 23:28:33 +0545

    fix: change dir to flexDirection

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
     return (
         <>
             <Header />
-            <Flex as={"main"} bg={"white.200"} minH={"90vh"} dir="column" justifyContent={"center"} py={{ base: "16px", md: 10 }} px={"16px"} gap={10}>
+            <Flex as={"main"} bg={"white.200"} minH={"90vh"} flexDir="column" align={"center"} py={{ base: "16px", md: 10 }} px={"16px"} gap={16}>
                 {children}
             </Flex>
         </>
